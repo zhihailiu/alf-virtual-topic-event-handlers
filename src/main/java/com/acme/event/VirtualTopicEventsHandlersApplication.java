ddeff575d2035499b177a634b32013f8e3ace4b9
zhihailiu <zhihai.liu@gmail.com>
2023-05-31 16:32:48 -0400

    initial commit

---
+++ src/main/java/com/acme/event/VirtualTopicEventsHandlersApplication.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021-2021 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.event;
+
+import org.alfresco.event.sdk.handling.handler.OnNodeCreatedEventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+@SpringBootApplication
+public class VirtualTopicEventsHandlersApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VirtualTopicEventsHandlersApplication.class);
+
+    public static void main(final String[] args) {
+        SpringApplication.run(VirtualTopicEventsHandlersApplication.class, args);
+    }
+
+    /**
+     * This event handler definition illustrates how you can use Spring's {@link Order} annotation to sort the execution of event handlers.
+     */
+    @Bean
+    @Order(10)
+    public OnNodeCreatedEventHandler firstCustomNodeCreatedEventHandler() {
+        return repoEvent -> LOGGER.info("First Event handler triggered on node created - Event: {}", repoEvent);
+    }
+
+    /**
+     * This event handler definition illustrates how you can use Spring's {@link Order} annotation to sort the execution of event handlers.
+     */
+    @Bean
+    @Order(20)
+    public OnNodeCreatedEventHandler secondCustomNodeCreatedEventHandler() {
+        return repoEvent -> LOGGER.info("Second Event handler triggered on node created - Event: {}", repoEvent);
+    }
+}
---
+++ src/main/java/com/acme/event/handler/ContentTypeNodeCreatedHandler.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021-2021 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.event.handler;
+
+import org.alfresco.event.sdk.handling.filter.EventFilter;
+import org.alfresco.event.sdk.handling.filter.NodeTypeFilter;
+import org.alfresco.event.sdk.handling.handler.OnNodeCreatedEventHandler;
+import org.alfresco.event.sdk.model.v1.model.DataAttributes;
+import org.alfresco.event.sdk.model.v1.model.NodeResource;
+import org.alfresco.event.sdk.model.v1.model.RepoEvent;
+import org.alfresco.event.sdk.model.v1.model.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Sample event handler to demonstrate reacting to the creation of a cm:content node in the repository.
+ */
+@Component
+public class ContentTypeNodeCreatedHandler implements OnNodeCreatedEventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContentTypeNodeCreatedHandler.class);
+
+    @Override
+    public void handleEvent(final RepoEvent<DataAttributes<Resource>> repoEvent) {
+        LOGGER.info("A new node named {} of type cm:content has been created!", ((NodeResource) repoEvent.getData().getResource()).getName());
+    }
+
+    @Override
+    public EventFilter getEventFilter() {
+        return NodeTypeFilter.of("cm:content");
+    }
+}
