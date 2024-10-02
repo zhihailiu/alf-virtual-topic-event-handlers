d4d9b08a22ab26deeb4602093ec17e08604e824c
hello9world <hello9world@gmail.com>
2024-10-02 09:16:43 -0400

    Upgraded to alfresco-java-sdk 6.2.0; Added conditional configuration

--- README.md
+++ README.md
@@ -5,27 +5,16 @@ The "competing consumers" issue with Alfresco Java SDK Event API can be found [h
 Let's say I create an application that generates a thumbnail after a PDF document is uploaded. I deploy three instances of the application for high availablity and better performance. Upon a PDF document upload, each of the three instances will receive an event to create thumbnail, while I only need one of them to do that. This is because Alfresco Event API uses ActiveMQ Topic behind the scene.
 
 This sample application demonstrates how to implement competing consumers using ActiveMQ Virutal Topic.
-1. On Alfresco/ACS side
 
-Use Virtual Topic for the endpoint configuration
-```
-repo.event2.topic.endpoint=amqp:topic:VirtualTopic.FOO
-```
-Virtual Topic naming convention is 
-```
-VirtualTopic.<topic name>
-```
+- On Alfresco/ACS side
 
-2. In the application
+Use Virtual Topic for the endpoint configuration `repo.event2.topic.endpoint=amqp:topic:VirtualTopic.FOO`. Virtual Topic naming convention is `VirtualTopic.<topic name>`.
 
-Switch Topic to Queue in code. Use a configuration to let application(s) listen to the same Queue that the Virtual Topic writes to.
-```
-alfresco.events.queueName=Consumer.BAR.VirtualTopic.FOO
-```
-Consumer Queue of the Virtual Topic has the naming convention 
-```
-Consumer.<consumer name>.VirtualTopic.<topic name>
-```
+- In the application
+
+Switch Topic to Queue in code. Use a configuration to let application(s) listen to the same Queue that the Virtual Topic writes to `alfresco.events.queueName=Consumer.BAR.VirtualTopic.FOO`.
+
+Consumer Queue of the Virtual Topic has the naming convention `Consumer.<consumer name>.VirtualTopic.<topic name>`.
 
 ### References
 * [How does a Queue compare to a Topic](https://activemq.apache.org/how-does-a-queue-compare-to-a-topic)
@@ -39,21 +28,10 @@ Consumer.<consumer name>.VirtualTopic.<topic name>
 
 To properly build and run the project in a local environment it is required to have installed some tools.
 
-* Java 11:
-```bash
-$ java -version
-
-openjdk version "11.0.1" 2018-10-16
-OpenJDK Runtime Environment 18.9 (build 11.0.1+13)
-OpenJDK 64-Bit Server VM 18.9 (build 11.0.1+13, mixed mode)
-```
+* Java 17:
 
 * [Maven](https://maven.apache.org/install.html) version 3.3 or higher:
-```bash
-$ mvn -version
 
-Apache Maven 3.6.1 (d66c9c0b3152b2e69ee9bac180bb8fcc8e6af555; 2019-04-04T21:00:29+02:00)
-```
 
 * [Docker](https://docs.docker.com/install/) version 1.12 or higher:
 ```bash
@@ -74,7 +52,7 @@ docker-compose version 1.27.4, build 40524192
 This sample project local development environment is based on Docker, so a ```run.sh/run.bat``` utility script has been included in order to build, run or stop 
 easily. This script will require execution permissions, so add it if it hasn't.
 
-```bash
+```
 $ chmod +x run.sh
 ```
  
@@ -84,7 +62,7 @@ See [```run.sh```](run.sh) or [```run.bat```](run.bat) if you would like to know
 
 Build all and run:
 
-```bash
+```
 $ ./run.sh build_start
 ```
 
--- src/main/java/com/acme/event/config/EventsAutoConfiguration.java
+++ src/main/java/com/acme/event/config/EventsAutoConfiguration.java
@@ -1,29 +1,35 @@
 package com.acme.event.config;
 
-import javax.jms.Queue;
-
 import org.alfresco.event.sdk.autoconfigure.AlfrescoEventsAutoConfiguration;
 import org.alfresco.event.sdk.autoconfigure.AlfrescoEventsProperties;
 import org.alfresco.event.sdk.integration.EventChannels;
-import org.alfresco.event.sdk.model.v1.model.DataAttributes;
-import org.alfresco.event.sdk.model.v1.model.RepoEvent;
-import org.alfresco.event.sdk.model.v1.model.Resource;
+import org.alfresco.repo.event.v1.model.DataAttributes;
+import org.alfresco.repo.event.v1.model.RepoEvent;
+import org.alfresco.repo.event.v1.model.Resource;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.jms.dsl.Jms;
-import org.springframework.integration.transformer.GenericTransformer;
 
+import jakarta.jms.Queue;
+import jakarta.jms.Topic;
+
+/**
+ * Queue configuration to support competing consumers.
+ */
 @Configuration
+@ConditionalOnProperty(name = "alfresco.events.queue.enabled", havingValue = "true")
 public class EventsAutoConfiguration {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EventsAutoConfiguration.class);
@@ -34,21 +40,21 @@ public class EventsAutoConfiguration {
 	 * the Alfresco side.
 	 * 
 	 * For example, Alfresco has the following virtual topic definition <code>
-	 * repo.event2.topic.endpoint=amqp:topic:VirtualTopic.events2
+	 * repo.event2.topic.endpoint=amqp:topic:VirtualTopic.BAR
 	 * </code>
 	 * 
-	 * Then the SDK application should have <code>
-	 * alfresco.events.queueName=Consumer.FOO.VirtualTopic.events2
-	 * </code> where "FOO" can be any arbitrary string.
+	 * Then the SDK application should have
+	 * <code>alfresco.events.queue.enabled=true</code> to enable this configuration,
+	 * and <code>
+	 * alfresco.events.queue.name=Consumer.FOO.VirtualTopic.BAR
+	 * </code> to match the Queue name on ACS side. "FOO" can be any arbitrary
+	 * string.
 	 */
-	@Value("${alfresco.events.queueName:Consumer.FOO.VirtualTopic.events2}")
+	@Value("${alfresco.events.queue.name:Consumer.FOO.VirtualTopic.BAR}")
 	private String queueName;
 
 	@Autowired
 	private AlfrescoEventsProperties alfrescoEventsProperties;
-
-	@Autowired
-	private DirectChannel acsEventErrorChannel;
 
 	@Autowired
 	private GenericTransformer<String, RepoEvent<DataAttributes<Resource>>> acsEventTransformer;
@@ -56,7 +62,7 @@ public class EventsAutoConfiguration {
 	/**
 	 * See
 	 * {@link AlfrescoEventsAutoConfiguration#acsEventsListeningFlow(ActiveMQConnectionFactory)}
-	 * in Alfresco SDK.
+	 * in Alfresco SDK 6.2.0.
 	 * 
 	 * Use Queue instead of Topic as destination. "@Primary" annotation makes this
 	 * preference over the Topic implementation in Alfresco SDK during dependency
@@ -67,12 +73,18 @@ public class EventsAutoConfiguration {
 	 */
 	@Bean
 	@Primary
-	public IntegrationFlow acsEventsListeningFlow2(final ActiveMQConnectionFactory activeMQConnectionFactory) {
-		return IntegrationFlows
-				.from(Jms
-						.messageDrivenChannelAdapter(activeMQConnectionFactory).destination(
-								acsEventsQueue())
-						.errorChannel(acsEventErrorChannel))
+	public IntegrationFlow acsEventsListeningFlow2(final ActiveMQConnectionFactory activeMQConnectionFactory,
+			@Qualifier(EventChannels.ERROR) final DirectChannel acsEventErrorChannel, 
+			final Topic acsEventsTopic,
+			final GenericTransformer<String, RepoEvent<DataAttributes<Resource>>> acsEventTransformer) {
+
+		LOGGER.info("Queue based events listening flow");
+
+		return IntegrationFlow.from(
+				Jms.messageDrivenChannelAdapter(activeMQConnectionFactory)
+				// use Queue instead of Topic
+				.destination(acsEventsQueue())
+				.errorChannel(acsEventErrorChannel))
 				.transform(acsEventTransformer)
 				.routeToRecipients(route -> route
 						.recipient(EventChannels.SPRING_INTEGRATION,
